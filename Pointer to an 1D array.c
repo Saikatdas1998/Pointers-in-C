e1a8af8e344970b22825afa65a50ce60df8e6b97
সৈকত দাস <63183700+Saikatdas1998@users.noreply.github.com>
2021-04-17 09:47:19 +0530

    Update Pointer to an 1D array.c

--- Pointer to an 1D array.c
+++ Pointer to an 1D array.c
@@ -1,5 +1,3 @@
-/*Pointer to an 1D array*/
-
 #include<stdio.h>
 
 int main()
@@ -8,17 +6,17 @@ int main()
     int arr[10]={11,21,35,10,5,60,78,81,19,150};
      int *p=arr;
     //p=&arr; 
-    printf("%d\n",arr[0]);   //traditonal method to print the value of array*/
+    printf("%d\n",arr[0]);   //traditonal method to print the value of array
     /*Using the terminology of pointers*/
-    /*printf("%p\n",arr);  //base loction of an array arr
+    printf("%p\n",arr);  //base loction of an array arr
     printf("%d\n",*arr);  //value of array arr[0]
     printf("%p\n",(arr+1));  // this the address of next index number i n arr array arr[1]
-    printf("%d\n",*(arr+1)); //arr[1]=2
+    printf("%d\n",*(arr+1)); //arr[1]=21
     printf("%p\n",(arr+1)+2);  //address of arr[3]
     printf("%p\n",(arr+3));  //address of arr[3]
-    printf("%d\n",*((arr+1)+2));
-    //printf("%p\n",*(*(arr+1)+2));
-    
+    printf("%d\n",*((arr+1)+2));//output will be 10
+    //printf("%d\n",*(*(arr+1)+2));
+    printf("------------------------------------------------\n");
     /*Following code will give same output*/
     
     
@@ -26,6 +24,7 @@ int main()
     printf("%d\n",*p);
     printf("%p\n",(p+1));
     printf("%d\n",*(p+1));
+    printf("%p\n",(p+3));
     printf("%p\n",((p+1)+2));
     printf("%d\n",*((p+1)+2));
     //printf("%d\n",*(*(p+1)+2));
